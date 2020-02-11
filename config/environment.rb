f35d6ef29eb368c3332c7262e08e324d55e5edb1
dianakw8591 <dianakw@gmail.com>
2020-02-11 15:11:08 -0800

    seeds database with recarea data; db renamed to development.db

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 Dotenv.load
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/rec_reservations.db')
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
 
 require 'net/http'
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,31 +8,55 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
-class GetRequester
-    attr_reader :url
-    def initialize(url)
-        @url = url
-    end
 
-  
-  def get_response_body
-    uri = URI.parse(self.url)
-    response = Net::HTTP.get_response(uri)
-    response.body
-  end
-
-  def parse_json
-    JSON.parse(self.get_response_body)
-  end
+RecArea.delete_all
  
+# yosemite = "https://ridb.recreation.gov/api/v1/recareas/2991"
+# upper_pines =   "https://ridb.recreation.gov/api/v1/facilities/232447"
+
+rec_codes = ["2991", "2782", "2931", "2662"]
+# https://ridb.recreation.gov/api/v1/recareas/2991/facilities where 2991 is recarea id
+# "ParentRecAreaID": "2991",
+#             "FacilityName": "Camp 4",
+#               "FacilityID"
+#             "FacilityDescription":
+# "FacilityTypeDescription": "Campground",
+# ["RECDATE"][array_num]["FacilityName"...etc]
+
+def get_request_for_rec_data(url)
+    RestClient::Request.execute(method: :get, 
+    url: url,
+    headers: {
+        apiKey: ENV['API_KEY']
+        }
+    )
 end
 
-url =   "https://ridb.recreation.gov/api/v1/facilities/232447"
-r = RestClient::Request.execute(method: :get, 
-  url: url,
-  headers: {
-    apiKey: ENV['API_KEY']
-    }
-)
-binding.pry
+def parse_json(url)
+    JSON.parse(get_request_for_rec_data(url))
+end
+
+def seed_rec_area_row(rec_hash)
+    name = rec_hash["RecAreaName"]
+    id = rec_hash["RecAreaID"]
+    description = rec_hash["RecAreaDescription"]
+    RecArea.create(name: name, official_rec_area_id: id, description: description)
+end
+
+def populate_rec_table(id_array)
+    base_url = "https://ridb.recreation.gov/api/v1/recareas/"
+    id_array.each do |id|
+        url = base_url + id
+        rec_hash = parse_json(url)
+        seed_rec_area_row(rec_hash)
+    end
+end
+
+populate_rec_table(rec_codes)
+
+
+# yos = parse_json(yosemite)
+# seed_rec_area_row(yos)
+
+
+# binding.pry
