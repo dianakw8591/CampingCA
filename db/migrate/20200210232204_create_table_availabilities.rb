212a0a72695a7398d5c4c3b05cd4db018812d51d
dianakw8591 <dianakw@gmail.com>
2020-02-10 16:30:34 -0800

    updated t.datetime to t.date

--- db/migrate/20200210232204_create_table_availabilities.rb
+++ db/migrate/20200210232204_create_table_availabilities.rb
@@ -2,7 +2,7 @@ class CreateTableAvailabilities < ActiveRecord::Migration[5.2]
   def change
     create_table :availabilities do |t|
       t.integer :campground_id
-      t.datetime :date
+      t.date :date
       t.boolean :open?
       t.integer :sites_available
       # may need more columns for tracking updates - updated sites available, update time
