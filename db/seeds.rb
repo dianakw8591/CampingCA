a88933acd7068b7ccf559c255a91144c290a24d2
dianakw8591 <dianakw@gmail.com>
2020-02-11 16:52:00 -0800

    added fake seed data for availability

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,18 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
+require 'date'
 
 RecArea.delete_all
 Campground.delete_all
+Availability.delete_all
  
 # yosemite = "https://ridb.recreation.gov/api/v1/recareas/2991"
-upper_pines =   "https://ridb.recreation.gov/api/v1/facilities/232447"
+# upper_pines =   "https://ridb.recreation.gov/api/v1/facilities/232447"
 
 rec_codes = ["2991", "2782", "2931", "2662"]
 
-
+# API request and parsing
 def get_request_for_rec_data(url)
     RestClient::Request.execute(method: :get, 
     url: url,
@@ -30,6 +32,8 @@ end
 def parse_json(url)
     JSON.parse(get_request_for_rec_data(url))
 end
+
+# RecArea seeding
 
 def seed_rec_area_row(rec_hash)
     name = rec_hash["RecAreaName"]
@@ -47,13 +51,8 @@ def populate_rec_table(id_array)
     end
 end
 
-# https://ridb.recreation.gov/api/v1/recareas/2991/facilities where 2991 is recarea id
-# "ParentRecAreaID": "2991",
-#             "FacilityName": "Camp 4",
-#               "FacilityID"
-#             "FacilityDescription":
-# "FacilityTypeDescription": "Campground",
-# ["RECDATA"][array_num]["FacilityName"...etc]
+# Campground seeding
+
 def seed_campground_row(campground_hash, i)
     name = campground_hash["FacilityName"]
     id = campground_hash["FacilityID"]
@@ -68,17 +67,32 @@ def populate_campground_table(rec_id_array)
         url = base_url + id + "/facilities"
         campground_hash = parse_json(url)
         campground_hash["RECDATA"].each do |campground|
-            seed_campground_row(campground, id)
+            if campground["FacilityTypeDescription"] == "Campground"
+                seed_campground_row(campground, id)
+            end
+        end
+    end
+end
+
+# availability table - fake availability for now
+
+def populate_availability_table
+    Campground.all.each do |camp|
+        date = Date.today
+        while date < Date.today.next_month do
+            sites = Random.rand(10)
+            avail = Availability.create(date: date, open?: true, sites_available: sites)
+            camp.availabilities << avail
+            date = date.next
         end
     end
 end
 
 
 
-        # if ["FacilityTypeDescription"] == "Campground"
-
 populate_rec_table(rec_codes)
 populate_campground_table(rec_codes)
+populate_availability_table
 
 
 # binding.pry
