0b6df782db545f27fe3c49a2c4cb83078a5e5abf
dianakw8591 <dianakw@gmail.com>
2020-02-11 16:15:32 -0800

    seeds db with campground data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,13 @@ require 'open-uri'
 require 'json'
 
 RecArea.delete_all
+Campground.delete_all
  
 # yosemite = "https://ridb.recreation.gov/api/v1/recareas/2991"
-# upper_pines =   "https://ridb.recreation.gov/api/v1/facilities/232447"
+upper_pines =   "https://ridb.recreation.gov/api/v1/facilities/232447"
 
 rec_codes = ["2991", "2782", "2931", "2662"]
-# https://ridb.recreation.gov/api/v1/recareas/2991/facilities where 2991 is recarea id
-# "ParentRecAreaID": "2991",
-#             "FacilityName": "Camp 4",
-#               "FacilityID"
-#             "FacilityDescription":
-# "FacilityTypeDescription": "Campground",
-# ["RECDATE"][array_num]["FacilityName"...etc]
+
 
 def get_request_for_rec_data(url)
     RestClient::Request.execute(method: :get, 
@@ -52,11 +47,38 @@ def populate_rec_table(id_array)
     end
 end
 
+# https://ridb.recreation.gov/api/v1/recareas/2991/facilities where 2991 is recarea id
+# "ParentRecAreaID": "2991",
+#             "FacilityName": "Camp 4",
+#               "FacilityID"
+#             "FacilityDescription":
+# "FacilityTypeDescription": "Campground",
+# ["RECDATA"][array_num]["FacilityName"...etc]
+def seed_campground_row(campground_hash, i)
+    name = campground_hash["FacilityName"]
+    id = campground_hash["FacilityID"]
+    description = campground_hash["FacilityDescription"]
+    camp = Campground.create(name: name, official_facility_id: id, description: description)
+    RecArea.find_by(official_rec_area_id: i).campgrounds << camp
+end
+
+def populate_campground_table(rec_id_array)
+    base_url = "https://ridb.recreation.gov/api/v1/recareas/"
+    rec_id_array.each do |id|
+        url = base_url + id + "/facilities"
+        campground_hash = parse_json(url)
+        campground_hash["RECDATA"].each do |campground|
+            seed_campground_row(campground, id)
+        end
+    end
+end
+
+
+
+        # if ["FacilityTypeDescription"] == "Campground"
+
 populate_rec_table(rec_codes)
-
-
-# yos = parse_json(yosemite)
-# seed_rec_area_row(yos)
+populate_campground_table(rec_codes)
 
 
 # binding.pry
